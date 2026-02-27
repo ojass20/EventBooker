83981a7438e97bc2d96c4bdc51dc8d16b92348f3
ojass20 <ojassdhadiwal20@gmail.com>
2026-02-27 12:55:53 +0530

    Renamed eventRoutes.js

---
+++ routes/eventRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const eventController = require("./../controllers/eventController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(eventController.getAllEvents)
+  .post(eventController.createEvent);
+
+router
+  .route("/:id")
+  .get(eventController.getEvent)
+  .patch(eventController.updateEvent)
+  .delete(eventController.deleteEvent);
+
+module.exports = router;
