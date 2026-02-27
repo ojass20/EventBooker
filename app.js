a909275357bbc8ed049b35de3ac00e9ff4ca089d
ojass20 <ojassdhadiwal20@gmail.com>
2026-02-27 13:00:41 +0530

    Added app.js file

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const fs = require("fs");
+const express = require("express");
+const morgan = require("morgan");
 
-console.log("Hello from the Event Booker app");
+const eventRouter = require("./routes/eventRoutes");
+
+const app = express();
+app.set("query parser", "extended");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from the Event Booker app middleware!");
+  next();
+});
+
+app.use("/api/v1/events/", eventRouter);
+
+module.exports = app;
