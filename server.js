f962eeb5853d3e0cdb56c28f8f73624e85d4eb2c
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-03 19:19:03 +0530

    Modified server.js file to handle uncaught exceptions and rejections

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log("UNHANDLED EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const DB = process.env.DATABASE.replace("<db_password>", process.env.PASSWORD);
@@ -12,4 +18,12 @@ mongoose.connect(DB).then(() => {
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
