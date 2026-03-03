20ba0c978350cb02a6eee43d3c8776962b588a96
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-03 19:17:46 +0530

    Added errorController.js file

---
+++ controllers/errorController.js
@@ -0,0 +1,19 @@
+const { error } = require("node:console");
+const AppError = require("../utils/appError");
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "Error";
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  }
+};
