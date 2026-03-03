e1ea929dee071ae32158d9a76dcb7d40138134d1
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-03 19:30:49 +0530

    Modified eventController.js file for better error handling

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,111 +1,75 @@
 const Event = require("./../models/eventModel");
 const APIFeatures = require("./../utils/APIFeatures");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
-exports.getAllEvents = async (req, res, next) => {
-  try {
-    const features = new APIFeatures(Event.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const events = await features.query;
-    // const events = await Event.find({});
+exports.getAllEvents = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Event.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const events = await features.query;
+  // const events = await Event.find({});
 
-    res.status(200).json({
-      status: "Success",
-      results: events.length,
-      data: {
-        events: events,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Fail",
-      message: err,
-    });
+  res.status(200).json({
+    status: "Success",
+    results: events.length,
+    data: {
+      events: events,
+    },
+  });
+});
+
+exports.getEvent = catchAsync(async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
+  if (!event) {
+    return next(new AppError("No event found with given ID", 404));
   }
-};
+  res.status(200).json({
+    status: "Success",
+    data: {
+      event: event,
+    },
+  });
+});
 
-exports.getEvent = async (req, res, next) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    if (!event) {
-      return res.status(400).json({
-        status: "Fail",
-        message: "Invalid ID",
-      });
-    }
-    res.status(200).json({
-      status: "Success",
-      data: {
-        event: event,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Fail",
-      message: err,
-    });
-  }
-};
+exports.createEvent = catchAsync(async (req, res, next) => {
+  const newEvent = await Event.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      event: newEvent,
+    },
+  });
+});
 
-exports.createEvent = async (req, res, next) => {
-  try {
-    const newEvent = await Event.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        event: newEvent,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Fail",
-      message: err,
-    });
+exports.updateEvent = catchAsync(async (req, res, next) => {
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!event) {
+    return next(new AppError("No event found with given ID", 404));
   }
-};
+  res.status(200).json({
+    status: "Success",
+    data: {
+      event: event,
+    },
+  });
+});
 
-exports.updateEvent = async (req, res, next) => {
-  try {
-    const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!event) {
-      return res.status(400).json({
-        status: "Fail",
-        message: "Invalid ID",
-      });
-    }
-    res.status(200).json({
-      status: "Success",
-      data: {
-        event: event,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Fail",
-      message: err,
-    });
+exports.deleteEvent = catchAsync(async (req, res, next) => {
+  const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+  if (!deletedEvent) {
+    return next(new AppError("No event found with given ID", 404));
   }
-};
-
-exports.deleteEvent = async (req, res, next) => {
-  try {
-    const deletedEvent = await Event.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      message: "Following event has been successfully deleted",
-      data: {
-        event: deletedEvent,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Fail",
-      message: err,
-    });
-  }
-};
+  res.status(204).json({
+    status: "success",
+    message: "Following event has been successfully deleted",
+    data: {
+      event: deletedEvent,
+    },
+  });
+});
