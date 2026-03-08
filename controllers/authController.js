b6b5b3d0b8ee252a8e3f278451a7472f744bf192
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-09 00:42:32 +0530

    Implemented basic authorization upon login

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,3 +44,37 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return next(
+        new AppError("You are not logged in, please login to get access", 401),
+      );
+    }
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const freshUser = await User.findById(decoded.id);
+    if (!freshUser) {
+      return next(
+        new AppError("The user belonging to this token does not exist!", 401),
+      );
+    }
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+      return next(
+        new AppError(
+          "User recently changed password! Please login again.",
+          401,
+        ),
+      );
+    }
+
+    req.user = freshUser;
+    next();
+  }
+});
