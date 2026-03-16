f5bdde23e5cb5a6d5593613019ed1cee281a0627
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 19:06:58 +0530

    Refactored all handlers for events API

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,74 +2,10 @@ const Event = require("../models/eventModel.js");
 const APIFeatures = require("./../utils/APIFeatures");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const factory = require("./handlerFactory.js");
 
-exports.getAllEvents = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Event.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const events = await features.query;
-  // const events = await Event.find({});
-
-  res.status(200).json({
-    status: "Success",
-    results: events.length,
-    data: {
-      events: events,
-    },
-  });
-});
-
-exports.getEvent = catchAsync(async (req, res, next) => {
-  const event = await Event.findById(req.params.id).populate("reviews");
-  if (!event) {
-    return next(new AppError("No event found with given ID", 404));
-  }
-  res.status(200).json({
-    status: "Success",
-    data: {
-      event: event,
-    },
-  });
-});
-
-exports.createEvent = catchAsync(async (req, res, next) => {
-  const newEvent = await Event.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      event: newEvent,
-    },
-  });
-});
-
-exports.updateEvent = catchAsync(async (req, res, next) => {
-  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  if (!event) {
-    return next(new AppError("No event found with given ID", 404));
-  }
-  res.status(200).json({
-    status: "Success",
-    data: {
-      event: event,
-    },
-  });
-});
-
-exports.deleteEvent = catchAsync(async (req, res, next) => {
-  const deletedEvent = await Event.findByIdAndDelete(req.params.id);
-  if (!deletedEvent) {
-    return next(new AppError("No event found with given ID", 404));
-  }
-  res.status(204).json({
-    status: "success",
-    message: "Following event has been successfully deleted",
-    data: {
-      event: deletedEvent,
-    },
-  });
-});
+exports.getAllEvents = factory.getAll(Event);
+exports.createEvent = factory.createOne(Event);
+exports.getEvent = factory.getOne(Event, { path: "reviews" });
+exports.updateEvent = factory.updateOne(Event);
+exports.deleteEvent = factory.deleteOne(Event);
