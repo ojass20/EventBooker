92b4633efc1ae7f4595b476206a965b7a9c77e4e
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 23:41:47 +0530

    Implemented avg ratings calculation

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,12 +37,54 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index({ event: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: "user",
     select: "name photo",
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (eventId) {
+  const stats = await this.aggregate([
+    {
+      $match: { event: eventId },
+    },
+    {
+      $group: {
+        _id: "$event",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Event.findByIdAndUpdate(eventId, {
+      totalRatings: stats[0].nRating,
+      averageRatings: stats[0].avgRating,
+    });
+  } else {
+    await Event.findByIdAndUpdate(eventId, {
+      totalRatings: 0,
+      averageRatings: 4.5,
+    });
+  }
+};
+
+reviewSchema.post("save", function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.event);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  this.r.constructor.calcAverageRatings(this.r.event);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
