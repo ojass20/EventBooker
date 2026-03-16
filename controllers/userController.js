8ab58ac205af20504eeb97f6d9ab0829f9a8c2cd
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 19:26:07 +0530

    Correct code cleanup

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,18 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-// exports.getAllUsers = catchAsync(async (req, res, next) => {
-//   const users = await User.find();
-
-//   res.status(200).json({
-//     status: "success",
-//     results: users.length,
-//     data: {
-//       tours: users,
-//     },
-//   });
-// });
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -55,33 +43,12 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-// exports.getUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined",
-//   });
-// };
-
-// exports.createUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined",
-//   });
-// };
-
-// exports.updateUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not yet defined",
-//   });
-// };
-
-// exports.deleteUser = (req, res) => {
-//   res.status(500).json({
-//     status: "error",
-//     message: "This route is not defined. Please use /signup instead",
-//   });
-// };
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
