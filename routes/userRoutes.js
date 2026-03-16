a7e729996733a270348d4d5fcd4ae3f669c7a091
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 22:22:20 +0530

    Implemented missing authentication & authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,26 +10,21 @@ router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 
-router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword,
-);
+router.use(authController.protect);
 
-router.get(
-  "/me",
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
+router.patch("/updateMyPassword", authController.updatePassword);
 
-router.patch("/updateMe", authController.protect, userController.updateMe);
-router.delete("/deleteMe", authController.protect, userController.deleteMe);
+router.get("/me", userController.getMe, userController.getUser);
+
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteMe);
 
 router
   .route("/")
-  .get(authController.protect, userController.getAllUsers)
+  .get(userController.getAllUsers)
   .post(userController.createUser);
+
+router.use(authController.restrictTo("admin"));
 
 router
   .route("/:id")
