05da030091253b05256a1b658fb437b70bab4127
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 17:44:30 +0530

    Added reviewController.js file with get and create handlers

---
+++ controllers/reviewController.js
@@ -0,0 +1,39 @@
+const Review = require("./../models/reviewModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+exports.getReviews = catchAsync(async (req, res, next) => {
+  let filter = {};
+  if (req.params.eventId) filter = { event: req.params.eventId };
+  const reviews = await Review.find(filter);
+
+  res.status(200).json({
+    status: "success",
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+// Nested routes checks are handled by a middleware now
+exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  console.log(req);
+  if (!req.body.event) req.body.event = req.params.eventId;
+  if (!req.body.user) req.body.user = req.user.id;
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      newReview,
+    },
+  });
+});
+
+exports.setEventUserIds = (req, res, next) => {
+  if (!req.body.event) req.body.event = req.params.eventId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
