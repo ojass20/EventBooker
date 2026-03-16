042479edd20c3c21ce5c0cc9602bffba97a0e0f2
ojass20 <ojassdhadiwal20@gmail.com>
2026-03-16 14:15:04 +0530

    Implemented basic rate limiting

--- app.js
+++ app.js
@@ -5,15 +5,44 @@ const morgan = require("morgan");
 const AppError = require("./utils/appError");
 const errorHandler = require("./controllers/errorController");
 
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 const eventRouter = require("./routes/eventRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
-app.set("query parser", "extended");
+app.set("query parser", "extended"); // In Express 5, we need to set this so that we can parse complex queries in API URLs
+
+// Middleware to make req.query propertly of request mutable, by default in Express 5, this property is immutable
+// so it is not possible to use security middleware directly on req.query as they modify the request parameters
+app.use((req, res, next) => {
+  const rawQuery = { ...req.query };
+  Object.defineProperty(req, "query", {
+    value: rawQuery,
+    writable: true,
+    enumerable: true,
+    configurable: true,
+  });
+  next();
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Middleware for rate limiting
+const limiter = rateLimiter({
+  max: 100, // No. of requests from same IP
+  window: 60 * 60 * 1000, // Time frame in ms
+  message:
+    "Too many incomming requests from this IP, please try again after 1 hour",
+});
+app.use("/api", limiter);
+
 app.use(express.json());
 
 app.use((req, res, next) => {
